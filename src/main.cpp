03e98c7d3a8ab8c74c364c6cb12a5b01328239de
chenlei_mac <ruyueshi@qq.com>
2020-08-08 21:07:23 +0800

    modify solution012: Integer to Roman

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
-#include "src/solution013.h"
+#include "src/solution012.h"
 
 int main() {
-    test_solution013();
+    test_solution012();
     return 0;
 }
--- src/solution012.h
+++ src/solution012.h
@@ -7,13 +7,34 @@
 #define LEETCODE_CPP_SOLUTION012_H
 
 #include <iostream>
+#include <string>
 
 class solution012 {
 public:
     std::string intToRoman(int num) {
-
+        int key[13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        std::string value[13] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        std::string ans = "";
+        int index = 0;
+        while (index < 13) {
+            if (num >= key[index]) {
+                ans += value[index];
+                num -= key[index];
+            } else {
+                index++;
+            }
+        }
+        return ans;
     }
 };
 
+void test_solution012() {
+    solution012 s;
+    std::cout << s.intToRoman(3) << std::endl;
+    std::cout << s.intToRoman(4) << std::endl;
+    std::cout << s.intToRoman(9) << std::endl;
+    std::cout << s.intToRoman(58) << std::endl;
+    std::cout << s.intToRoman(1994) << std::endl;
+}
 
 #endif //LEETCODE_CPP_SOLUTION012_H
