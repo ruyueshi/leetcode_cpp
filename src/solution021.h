3ed5c4ca0af3415af2d4aebeb991fa51437733b3
chenlei_mac <ruyueshi@qq.com>
2020-08-22 20:29:36 +0800

    After optimized solution021, the algorithm takes up less memory.

--- src/solution021.h
+++ src/solution021.h
@@ -19,8 +19,8 @@ struct ListNode {
 
 class solution021 {
 public:
-    // 8 ms, 14 MB
-    ListNode* mergeTwoList1(ListNode* l1, ListNode* l2) {
+    // 8 ms, 14.5 MB
+    ListNode* mergeTwoLists1(ListNode* l1, ListNode* l2) {
         if (!l1 || !l2) {
             return !l1 ? l2 : l1;
         }
@@ -65,33 +65,42 @@ public:
         return head;
     }
 
+    // 8 ms, 14.2 MB, which takes up less memory
     ListNode* mergeTwoLists2(ListNode* l1, ListNode* l2) {
-        ListNode *head = l1;
-        ListNode *p1 = l1, *p2 =l2;
-
-        if (p1->val > p2->val) {
-            head = p2;
-            p2 = p2->next;
-            head->next = p1;
-            p1 = head;
+        if (!l1 || !l2) {
+            return !l1 ? l2 : l1;
         }
+        ListNode *head, *p1, *p2;
+        if (l1->val <= l2->val) {
+            head = l1;
+            p1 = l1->next;
+            p2 = l2;
+        } else {
+            head = l2;
+            p1 = l1;
+            p2 = l2->next;
+        }
+        ListNode *pre = head;
+
         while (p1 || p2) {
             if (!p1) { // l1 traverse over
-//                p1->next = p2;
+                pre->next = p2;
                 break;
             } else if (!p2) { // l2 traverse over
+                pre->next = p1;
                 break;
             } else {
                 if (p1->val <= p2->val) {
-                    ListNode *t = p2;
+                    pre->next = p1;
+                    pre = pre->next;
+                    p1 = p1->next;
+                } else {
+                    pre->next = p2;
+                    pre = pre->next;
                     p2 = p2->next;
-                    t->next = p1->next;
-                    p1->next = t;
-                    p1 = t;
                 }
             }
         }
-
         return head;
     }
 };
@@ -109,7 +118,7 @@ void test_solution021() {
         p2->next = new ListNode(num2[i]);
         p2 = p2->next;
     }
-    ListNode *ans = s.mergeTwoList1(l1, l2);
+    ListNode *ans = s.mergeTwoLists2(l1, l2);
     while (ans) {
         std::cout << ans->val << "->";
         ans = ans->next;
