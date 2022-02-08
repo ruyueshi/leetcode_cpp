3c5bca438852d1cd6bed50004a84a83810dd2337
chenlei <ruyueshi@qq.com>
2022-02-08 17:45:49 +0800

    Add offer58: Rotate string left.

---
+++ src/offer/offer58.cpp
@@ -0,0 +1,21 @@
+/**
+ * offer 58. 左旋转字符串
+ * Source: https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof/
+ */
+
+#include <iostream>
+#include <string>
+
+class Solution {
+public:
+    std::string reverseLeftWords(std::string s, int n) {
+        return s.substr(n) + s.substr(0, n);
+    }
+};
+
+int main() {
+    Solution s;
+    std::string str = "lrloseumgh";
+    std::cout << s.reverseLeftWords(str, 6) << std::endl;
+    return 0;
+}
