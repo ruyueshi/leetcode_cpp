6f308b5d53610646324c5403d28c58344e3bd438
chenlei <ruyueshi@qq.com>
2022-02-11 11:41:12 +0800

    Add offer28: Symmetric tree: using recursive algorithm to judge if a tree is symmetric.

---
+++ src/offer/offer28.cpp
@@ -0,0 +1,48 @@
+/**
+ * offer 28. 对称的二叉树
+ * Source: https://leetcode-cn.com/problems/dui-cheng-de-er-cha-shu-lcof/
+ */
+
+#include <iostream>
+#include <stack>
+#include <queue>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+// 递归
+class Solution {
+public:
+    bool isSymmetric(TreeNode *root) {
+        if (!root)
+            return false;
+        return this->isSymmetric(root->left, root->right);
+    }
+
+private:
+    bool isSymmetric(TreeNode *t1, TreeNode *t2) {
+        if (t1 == nullptr && t2 == nullptr)
+            return true;
+        else if (t1 == nullptr || t2 == nullptr)
+            return false;
+        else
+            return t1->val == t2->val && isSymmetric(t1->left, t2->right) && isSymmetric(t1->right, t2->left);
+    }
+};
+
+int main() {
+    auto root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(2);
+    root->left->left = new TreeNode(3);
+    root->left->right = new TreeNode(4);
+    root->right->left = new TreeNode(4);
+    root->right->right = new TreeNode(3);
+    std::cout << Solution().isSymmetric(root) << std::endl;
+    return 0;
+}
