098997c85a3e900de38a885fcca4c3ad1b1a723e
chenlei <ruyueshi@qq.com>
2022-02-15 21:09:12 +0800

    Add offer10-3: Maximum profit in stock: using dynamic programming algorithm.

---
+++ src/offer/offer10-3.cpp
@@ -0,0 +1,30 @@
+/**
+ * offer 10-2. 青蛙跳台阶问题
+ * Source: https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof/
+ */
+
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    int maxProfit(std::vector<int> &prices) {
+        if (prices.size() <= 1)
+            return 0;
+        int min_price = prices[0], cur = 0;  // min_price表示前i天里最小的股票价格
+        for (size_t i = 1; i < prices.size(); i++) {
+            cur = std::max(cur, (prices[i] - min_price));
+            min_price = std::min(min_price, prices[i]);
+        }
+        return cur;
+    }
+};
+
+int main() {
+    Solution s;
+    std::vector<int> a = {7, 1, 5, 3, 6, 4};
+    std::cout << s.maxProfit(a) << std::endl;
+    a = {7, 6, 5, 4, 3, 2, 1};
+    std::cout << s.maxProfit(a) << std::endl;
+    return 0;
+}
