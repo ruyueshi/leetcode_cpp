ba4cae80635645720387a1a47e12623f1b11b3be
chenlei <ruyueshi@qq.com>
2022-02-17 11:41:31 +0800

    Add offer22: Find the kth node from the end in the list.

---
+++ src/offer/offer22.cpp
@@ -0,0 +1,45 @@
+/**
+ * offer 18. 链表中倒数第k个节点
+ * Source: https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/
+ */
+
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *getKthFromEnd(ListNode *head, int k) {
+        ListNode *pre = head, *cur = head;
+        for (int i = 0; i < k && cur; i++)
+            cur = cur->next;
+        while (cur) {
+            cur = cur->next;
+            pre = pre->next;
+        }
+        return pre;
+    }
+};
+
+int main() {
+    Solution s;
+    ListNode *head = new ListNode(1);
+    int num[4] = {2, 3, 4, 5};
+    auto p = head;
+    for (auto i: num) {
+        p->next = new ListNode(i);
+        p = p->next;
+    }
+    auto res = s.getKthFromEnd(head, 2);
+    while (res) {
+        std::cout << res->val << "->";
+        res = res->next;
+    }
+    std::cout << std::endl;
+    return 0;
+}
