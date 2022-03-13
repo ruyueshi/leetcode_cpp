c33b79683139ea3d390c785ba806bc9994fe100f
chenlei <ruyueshi@qq.com>
2022-03-13 21:16:27 +0800

    Update 121_BestTimeToBuyAndSellStock: 完全动态规划。

--- src/stage3/dynamic_programming/121_BestTimeToBuyAndSellStock.cpp
+++ src/stage3/dynamic_programming/121_BestTimeToBuyAndSellStock.cpp
@@ -15,7 +15,7 @@ public:
         std::vector<int> dp(prices.size(), 0);
         int min_price = prices[0];
         for (int i = 1; i < prices.size(); i++) {
-            dp[i] = std::max(dp[i-1], prices[i] - min_price);
+            dp[i] = std::max(dp[i - 1], prices[i] - min_price);
             min_price = std::min(min_price, prices[i]);
         }
         return dp.back();
@@ -37,9 +37,42 @@ public:
     }
 };
 
+// 完全动态规划：
+class SolutionV3 {
+public:
+    int maxProfit(std::vector<int> &&prices) {
+        int n = prices.size();
+        int dp[n][2];
+        dp[0][0] = 0, dp[0][1] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            // 第i天不持有股票
+            dp[i][0] = std::max(dp[i - 1][0], dp[i - 1][1] + prices[i]);  // std::max(现在不持有股票并且前一天也不持有股票, 前一天持有股票且今天把股票卖了)
+            // 第i天持有股票
+            dp[i][1] = std::max(dp[i - 1][1], -prices[i]);  // std::max(现在持有股票并且前一天也持有股票, 前一天不持有股票且今天买入股票)
+        }
+        return dp[n - 1][0];  // 最后一天手里应当不持有股票
+    }
+};
+
+// 优化V3
+class SolutionV4 {
+public:
+    int maxProfit(std::vector<int> &&prices) {
+        int dp_i_0 = 0, dp_i_1 = -prices[0];
+        for (int i = 1; i < prices.size(); i++) {
+            // 第i天不持有股票
+            dp_i_0 = std::max(dp_i_0, dp_i_1 + prices[i]);  // std::max(现在不持有股票并且前一天也不持有股票, 前一天持有股票且今天把股票卖了)
+            // 第i天持有股票
+            dp_i_1 = std::max(dp_i_1, -prices[i]);  // std::max(现在持有股票并且前一天也持有股票, 前一天不持有股票且今天买入股票)
+        }
+        return dp_i_0;  // 最后一天手里应当不持有股票
+    }
+};
+
 int main() {
-    SolutionV2 s;
+    SolutionV4 s;
     std::cout << s.maxProfit({2, 4, 1}) << std::endl;
     std::cout << s.maxProfit({3, 2, 6, 5, 0, 3}) << std::endl;
+    std::cout << s.maxProfit({7, 1, 5, 3, 6, 4}) << std::endl;
     return 0;
 }
