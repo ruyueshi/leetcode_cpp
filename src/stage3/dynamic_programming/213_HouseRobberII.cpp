f7b3e17dcfe8a1ffb996263f47339dcf42dc326d
chenlei <ruyueshi@qq.com>
2022-03-15 17:08:11 +0800

    Update 213_HouseRobberII: 动态规划.

--- src/stage3/dynamic_programming/213_HouseRobberII.cpp
+++ src/stage3/dynamic_programming/213_HouseRobberII.cpp
@@ -6,30 +6,53 @@
 #include <iostream>
 #include <vector>
 
-class Solution {
+// 动态规划
+class SolutionV1 {
 public:
     int rob(std::vector<int> &&nums) {
         int n = nums.size();
+        if (n == 1)
+            return nums[0];
+        return std::max(robDp(nums, 0, n - 2), robDp(nums, 1, n - 1));
+    }
+
+    int robDp(const std::vector<int> &nums, int start, int end) {
+        int n = end - start + 1;
+        if (n == 1)
+            return nums[start];
         int dp[n];
-        for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                dp[i] = nums[0];
-            } else if (i == 1) {
-                dp[i] = std::max(nums[0], nums[1]);
-            } else if (i == 2) {
-                dp[i] = std::max(dp[1], nums[2]);
-            } else if (i == 3) {
-                dp[i] = std::max(nums[0] + nums[2], nums[1] + nums[3]);
-            } else {
-                dp[i] = std::max(std::max(dp[i - 1], dp[0]), std::max(dp[i - 2] + nums[i], dp[1] + nums[i]));
-            }
+        dp[0] = nums[start], dp[1] = std::max(nums[start], nums[start + 1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = std::max(dp[i - 1], dp[i - 2] + nums[start + i]);
         }
         return dp[n - 1];
     }
 };
 
+// 优化V1
+// 压缩空间
+class SolutionV2 {
+public:
+    int rob(std::vector<int> &&nums) {
+        int n = nums.size();
+        if (n == 1)
+            return nums[0];
+        return std::max(robDp(nums, 0, n - 2), robDp(nums, 1, n - 1));
+    }
+
+    int robDp(const std::vector<int> &nums, int start, int end) {
+        int dp_i = 0, dp_i_1 = 0, dp_i_2 = 0;
+        for (int i = start; i <= end; i++) {
+            dp_i = std::max(dp_i_1, dp_i_2 + nums[i]);
+            dp_i_2 = dp_i_1;
+            dp_i_1 = dp_i;
+        }
+        return dp_i;
+    }
+};
+
 int main() {
-    Solution s;
+    SolutionV2 s;
     std::cout << s.rob({200, 3, 140, 20, 10}) << std::endl;
     std::cout << s.rob({2, 3, 2}) << std::endl;
     std::cout << s.rob({1, 2, 3, 1}) << std::endl;
