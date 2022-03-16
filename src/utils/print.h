2ca560f8649d1938db31e6d551d8060c2212d525
chenlei <ruyueshi@qq.com>
2022-03-16 10:29:02 +0800

    Add debug function for using recursion method.

--- src/utils/print.h
+++ src/utils/print.h
@@ -4,11 +4,19 @@
 #include <iostream>
 #include <vector>
 
+// print output result
 template<typename T>
 static void print_vector(std::vector<T> nums) {
     for (const auto &i: nums)
         std::cout << i << ",";
     std::cout << "\b\n";
+}
+
+// debug in recursion
+static void print_indent(int n) {
+    for (int i = 0; i < n; i++) {
+        printf("   ");
+    }
 }
 
 #endif //LEETCODE_CPP_PRINT_H
