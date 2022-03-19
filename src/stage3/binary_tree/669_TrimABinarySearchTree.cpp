e335b12e0e2ecdca9451539e07e68ec5e6634d79
chenlei <ruyueshi@qq.com>
2022-03-19 16:06:13 +0800

    Add 669_TrimABinarySearchTree: 递归。

--- src/stage3/binary_tree/669_TrimABinarySearchTree.cpp
+++ src/stage3/binary_tree/669_TrimABinarySearchTree.cpp
@@ -3,28 +3,28 @@
  * Source: https://leetcode-cn.com/problems/diameter-of-binary-tree/
  */
 
-#include <iostream>
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+#include "src/utils/tree.h"
 
 class Solution {
 public:
-    TreeNode* trimBST(TreeNode* root, int low, int high) {
-
+    TreeNode *trimBST(TreeNode *root, int low, int high) {
+        if (!root)
+            return nullptr;
+        if (root->val < low) {
+            return trimBST(root->right, low, high);
+        } else if (root->val > high) {
+            return trimBST(root->left, low, high);
+        } else {
+            root->left = trimBST(root->left, low, high);
+            root->right = trimBST(root->right, low, high);
+            return root;
+        }
     }
 };
 
 int main() {
-
+    Solution s;
+    print_tree1(s.trimBST(construct_tree({1, 0, 2}), 1, 2));
+    print_tree1(s.trimBST(construct_tree({3, 0, 4, -1, 2, -1, -1, 1}), 1, 3));
     return 0;
 }
--- src/utils/tree.h
+++ src/utils/tree.h
@@ -2,8 +2,8 @@
 #define LEETCODE_CPP_TREE_H
 
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 
 struct TreeNode {
     int val;
@@ -18,16 +18,99 @@ struct TreeNode {
 };
 
 /*
- * nodes = {3,0,4,INT_MIN,2,INT_MIN,INT_MIN,1}; // INT_MIN means null node;
+ * nodes = {3,0,4,-1,2,-1,-1,1}; // -1 means null node;
  */
-static TreeNode *construct_tree(std::vector<int> nodes) {
+static TreeNode *construct_tree(const std::vector<int> &nodes) {
     if (nodes.size() == 0)
         return nullptr;
-    auto root = new TreeNode(nodes[0]), p = root;
+    auto root = new TreeNode(nodes[0]);
     std::queue<TreeNode *> q;
+    q.push(root);
     for (int i = 1; i < nodes.size(); i++) {
-        if ()
+        auto p = q.front();
+        q.pop();
+        if (nodes[i] >= 0) {
+            p->left = new TreeNode(nodes[i]);
+            q.push(p->left);
+        }
+        if (++i >= nodes.size())
+            break;
+        if (nodes[i] >= 0) {
+            p->right = new TreeNode(nodes[i]);
+            q.push(p->right);
+        }
     }
+    return root;
 }
+
+static void print_tree1(TreeNode *root) {
+    if (!root) {
+        std::cout << "[]" << std::endl;
+        return;
+    }
+    std::queue<TreeNode *> q;
+    q.push(root);
+    std::cout << "[" << root->val;
+    while (!q.empty()) {
+        auto p = q.front();
+        q.pop();
+        if (!p->left && !p->right && q.empty())
+            break;
+        if (p->left) {
+            std::cout << "," << p->left->val;
+            q.push(p->left);
+        } else {
+            std::cout << ",null";
+        }
+        if (p->right) {
+            std::cout << "," << p->right->val;
+            q.push(p->right);
+        } else {
+            std::cout << ",null";
+        }
+    }
+    std::cout << "]\n";
+}
+
+// static void traceback(TreeNode *root, int level, std::vector<std::string> &tree) {
+//     if (level >= tree.size() / 2) {
+//         tree.emplace_back(std::to_string(root->val));
+//     }
+//
+//     if (root->left) {
+//         if (level >= tree.size() / 2) {
+//             tree.emplace_back("|");
+//         } else {
+//             tree[2 * level + 1] += "|";
+//         }
+//         traceback(root->left, level + 1, tree);
+//     } else {
+//         if (level >= tree.size() / 2) {
+//             tree.emplace_back(" ");
+//         } else {
+//             tree[2 * level + 1] += " ";
+//         }
+//     }
+//
+//     if (root->right) {
+//         if (level >= tree.size() / 2) {
+//             tree.emplace_back("\\");
+//         } else {
+//             tree[2 * level + 1] += "\\";
+//         }
+//         traceback(root->right, level + 1, tree);
+//     } else {
+//         if (level >= tree.size() / 2) {
+//             tree.emplace_back("\\");
+//         } else {
+//             tree[2 * level + 1] += "\\";
+//         }
+//     }
+// }
+//
+// static void print_tree2(TreeNode *root) {
+//     std::vector<std::string> tree;
+//     traceback(root, 0, tree);
+// }
 
 #endif //LEETCODE_CPP_TREE_H
