e17b2e9a32b19a7dd73f446b5e8fc0e1fb3d5c7b
chenlei <ruyueshi@qq.com>
2022-03-19 12:17:02 +0800

    Update solution4: 树形动态规划!

--- src/test/meituan/solution4.cpp
+++ src/test/meituan/solution4.cpp
@@ -1,39 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <climits>
+#include "memory.h"
+
+#define MAX_N 301
+
+typedef std::vector<std::vector<std::vector<int>>> threeD_array;
+typedef std::vector<std::vector<int>> twoD_array;
 
 int N;
-std::vector<int> nodes;
+// std::vector<int> nodes;
+int nodes[MAX_N];
+int dp[MAX_N][MAX_N][MAX_N];
 
 int traceback(int root_index, int left_edge, int right_edge) {
-    int min_left = INT_MAX, min_right = INT_MAX;
+    if (left_edge > right_edge)
+        return 0;
+    if (dp[root_index][left_edge][right_edge] != -1)
+        return dp[root_index][left_edge][right_edge];
 
-    // 寻找左子树的最小开销
-    for (int i = left_edge; i <= root_index - 1; i++) {
-        min_left = std::min(min_left, nodes[root_index] * nodes[i] + traceback(i, left_edge, root_index - 1));
-    }
-
-    // 寻找右子树的最小开销
-    for (int i = root_index + 1; i <= right_edge; i++) {
-        min_right = std::min(min_right, nodes[root_index] * nodes[i] + traceback(i, root_index + 1, right_edge));
-    }
-
-    return (min_left == INT_MAX ? 0 : min_left) + (min_right == INT_MAX ? 0 : min_right);
-}
-
-int get_best_tree() {
     int res = INT_MAX;
-    for (int i = 0; i < N; i++) {
-        res = std::min(res, traceback(i, 0, N - 1));
+    for (int i = left_edge; i <= right_edge; i++) {
+        int min_left = traceback(i, left_edge, i - 1);
+        int min_right = traceback(i, i + 1, right_edge);
+        res = std::min(res, min_left + min_right + nodes[i] * nodes[root_index]);
     }
+    dp[root_index][left_edge][right_edge] = res;
     return res;
 }
 
 int main() {
     std::cin >> N;
-    nodes.resize(N);
-    for (int i = 0; i < N; i++)
+    // nodes.resize(N + 1);
+    nodes[0] = 0;
+    for (int i = 1; i <= N; i++)
         std::cin >> nodes[i];
-    std::cout << get_best_tree() << std::endl;
+    // threeD_array dp(N + 1, twoD_array(N + 1, std::vector<int>(N + 1, -1)));  // dp[i][j][k] 表示以i为根节点、j-k为子树节点时的最小开销
+    memset(dp, -1, sizeof dp);
+    std::cout << traceback(0, 1, N) << std::endl;  // 0为虚拟节点
     return 0;
 }
