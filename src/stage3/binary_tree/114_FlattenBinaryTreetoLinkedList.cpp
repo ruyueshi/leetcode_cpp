97e8da0386658ab2e6fd95bf14990cba7a252aeb
chenlei <ruyueshi@qq.com>
2022-03-19 20:12:02 +0800

    Add 114_FlattenBinaryTreetoLinkedList: 巧妙的后序遍历。

---
+++ src/stage3/binary_tree/114_FlattenBinaryTreetoLinkedList.cpp
@@ -0,0 +1,36 @@
+/**
+ * 538. Convert BST to Greater Tree
+ * Source: https://leetcode-cn.com/problems/convert-bst-to-greater-tree/
+ */
+
+#include "src/utils/tree.h"
+
+class Solution {
+public:
+    void flatten(TreeNode *root) {
+        if (!root)
+            return;
+        flatten(root->left);
+        flatten(root->right);
+        auto left = root->left;
+        auto right = root->right;
+        root->right = left;
+        root->left = nullptr;
+        auto p = root;
+        while (p->right) {
+            p = p->right;
+        }
+        p->right = right;
+    }
+};
+
+int main() {
+    Solution s;
+    auto root = construct_tree({1, 2, 5, 3, 4, INT_MIN, 6});
+    s.flatten(root);
+    print_tree1(root);
+    root = construct_tree({0});
+    s.flatten(root);
+    print_tree1(root);
+    return 0;
+}
