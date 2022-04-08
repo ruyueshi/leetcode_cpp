6d91b3ed14a21f8f43119a7aae459885eb8693d4
chenlei <ruyueshi@qq.com>
2022-04-08 11:00:20 +0800

    Update 300_LongestIncreasingSubsequence: 二分查找-最长递增子序列.

--- src/stage3/dynamic_programming/300_LongestIncreasingSubsequence.cpp
+++ src/stage3/dynamic_programming/300_LongestIncreasingSubsequence.cpp
@@ -6,11 +6,13 @@
 #include <iostream>
 #include <vector>
 
-class Solution {
+// V1: 动态规划
+// 时间复杂度：O(n^2)
+class SolutionV1 {
 public:
     int lengthOfLIS(std::vector<int> &nums) {
         int n = nums.size();
-        std::vector<int> dp(n, 1);
+        std::vector<int> dp(n, 1);  // dp[i]：以nums[i]结尾的递增子序列的长度
         int res = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
@@ -23,8 +25,41 @@ public:
     }
 };
 
+// V2: 二分查找
+// 时间复杂度：O(nlogn)
+// https://labuladong.github.io/algo/3/24/69/
+class SolutionV2 {
+public:
+    int lengthOfLIS(std::vector<int> &nums) {
+        int n = nums.size();
+        std::vector<int> top;
+        int heap_num = 0;
+        for (int i = 0; i < n; i++) {
+            int candi = nums[i];
+            int left = 0, right = heap_num;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (top[mid] == candi) {
+                    left = mid;
+                    break;
+                } else if (top[mid] > candi) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            if (left == heap_num) {
+                heap_num++;
+                top.push_back(candi);
+            } else
+                top[left] = candi;
+        }
+        return heap_num;
+    }
+};
+
 int main() {
-    Solution s;
+    SolutionV2 s;
     std::vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};
     std::cout << s.lengthOfLIS(nums) << std::endl;
     return 0;
