2cd52a1636cad8765ed58cce5c3be3cb096a760d
chenlei <ruyueshi@qq.com>
2022-09-16 16:47:11 +0800

    Update 086_PartitionList: use two list to record, and merge the two list lastly.

--- src/stage3/linked_lists/086_PartitionList.cpp
+++ src/stage3/linked_lists/086_PartitionList.cpp
@@ -18,7 +18,9 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-class Solution {
+// 在原表上操作
+// 速度更快
+class SolutionV1 {
 public:
     ListNode *partition(ListNode *head, int x) {
         ListNode dummy(-1, head);
@@ -39,5 +41,30 @@ public:
             p = q;
         }
         return dummy.next;
+    }
+};
+
+// 记录两个表，最后合并两个表
+// V2 更清晰
+class SolutionV2 {
+public:
+    ListNode *partition(ListNode *head, int x) {
+        ListNode dummy1, dummy2;
+        ListNode *p1 = &dummy1, *p2 = &dummy2;
+        auto p = head;
+        while (p) {
+            if (p->val < x) {
+                p1->next = p;
+                p1 = p;
+            } else {
+                p2->next = p;
+                p2 = p;
+            }
+            auto temp = p->next;
+            p->next = nullptr;
+            p = temp;
+        }
+        p1->next = dummy2.next;
+        return dummy1.next;
     }
 };
